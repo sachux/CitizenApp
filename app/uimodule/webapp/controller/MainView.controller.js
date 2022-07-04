253126aefd8236702431c8c7942799580c314a0d
sachux <sachinkalpetta@gmail.com>
2022-07-05 00:01:18 +0530

    Generated UI code

---
+++ app/uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "./BaseController"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("com.sachinscloud.citizenui.controller.MainView", {
+            onInit: function () {
+
+            }
+        });
+    });
